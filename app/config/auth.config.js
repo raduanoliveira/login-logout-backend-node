cd88356862262e053c684963061674973435ff85
Raduan de Oliveira Bandeira <raduan.fcc@gmail.com>
2023-02-07 11:02:48 -0300

    finalizando frontend

---
+++ app/config/auth.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    secret: 'raduan-secret-key'
+}
--- server.js
+++ server.js
@@ -4,16 +4,48 @@ const cors = require('cors')
 const app = express()
 
 var corsOptions = {
-    origin: 'http://localhost:8081'
+    origin: 'http://localhost:3000'
 }
 
 app.use(cors(corsOptions))
 
 app.use(express.json())
 
+const db = require('./app/models')
+
+const Role = db.role
+
+function initial(){
+    Role.create({
+        id: 1,
+        name:'user'
+    })
+
+    Role.create({
+        id: 2,
+        name:'moderator'
+    })
+
+    Role.create({
+        id: 3,
+        name:'admin'
+    })
+}
+
+db.sequelize.sync({force:true}).then(()=>{
+    console.log('Drop and Resync Db')
+    initial()
+})
+
+//for production
+// db.sequelize.sync()
+
 app.get('/',(req,res)=>{
     res.json({message: 'Bem vindo a app login-logout'})
 })
+
+require('./app/routes/auth.routes')(app)
+require('./app/routes/user.routes')(app)
 
 const PORT = process.env.PORT || 8000
 
